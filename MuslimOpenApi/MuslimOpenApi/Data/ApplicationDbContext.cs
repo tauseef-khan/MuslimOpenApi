bc95d7450dd9a72bbfeed1138a20644795d0ed6e
tauseef-khan <tdakhan@gmail.com>
2020-02-26 22:22:57 +0000

    installed EF Core and EF PostgreSQL package and created Data and Models folders

---
+++ MuslimOpenApi/MuslimOpenApi/Data/ApplicationDbContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace MuslimOpenApi.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions options) : base(options)
+        {
+        }
+    }
+}
