0bf19f0f616bbd55373ced24f1a8a1b0ef72badf
tauseef-khan <tdakhan@gmail.com>
2020-03-22 19:49:59 +0000

    removed unnecessary usings and added model to DB context

--- MuslimOpenApi/MuslimOpenApi/Data/ApplicationDbContext.cs
+++ MuslimOpenApi/MuslimOpenApi/Data/ApplicationDbContext.cs
@@ -1,9 +1,12 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using MuslimOpenApi.Models;
 
 namespace MuslimOpenApi.Data
 {
     public class ApplicationDbContext : DbContext
     {
+        public DbSet<PrayerTime> PrayerTimes { get; set; }
+
         public ApplicationDbContext(DbContextOptions options) : base(options)
         {
         }
