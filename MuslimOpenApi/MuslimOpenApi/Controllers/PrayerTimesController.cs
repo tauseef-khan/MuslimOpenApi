622be30dfd2ecee292f0537672794e5ae2649628
tauseef-khan <tdakhan@gmail.com>
2020-03-22 19:48:35 +0000

    added PrayerTime model and controller

---
+++ MuslimOpenApi/MuslimOpenApi/Controllers/PrayerTimesController.cs
@@ -0,0 +1,61 @@
+﻿using System.Linq;
+using Microsoft.AspNetCore.Mvc;
+using MuslimOpenApi.Data;
+
+namespace MuslimOpenApi.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class PrayerTimesController : ControllerBase
+    {
+        private readonly ApplicationDbContext _context;
+
+        public PrayerTimesController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/PrayerTimes
+        [HttpGet]
+        public IActionResult Get()
+        {
+            //return new string[] { "value1", "value2" };
+            var abc = _context.PrayerTimes.Count();
+            var prayerTimeObject = _context.PrayerTimes.Where(x => x.Date.Contains("01-01-2020")).ToList();
+
+            if (!prayerTimeObject.Any())
+            {
+                return NotFound();
+            }
+
+            return Ok(prayerTimeObject);
+        }
+
+        /*
+        // GET: api/PrayerTimes/5
+        [HttpGet("{id}", Name = "Get")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST: api/PrayerTimes
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT: api/PrayerTimes/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE: api/ApiWithActions/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+        */
+    }
+}
---
+++ MuslimOpenApi/MuslimOpenApi/Models/PrayerTime.cs
@@ -0,0 +1,33 @@
+﻿namespace MuslimOpenApi.Models
+{
+    public class PrayerTime
+    {
+        public int ID { get; set; }
+
+        public string Date { get; set; }
+
+        public string FajrBegin { get; set; }
+
+        public string FajrJamaah { get; set; }
+
+        public string Sunrise { get; set; }
+
+        public string ZuhrBegin { get; set; }
+
+        public string ZuhrJamaah { get; set; }
+
+        public string AsrMithl1 { get; set; }
+
+        public string AsrMithl2 { get; set; }
+
+        public string AsrJamaah { get; set; }
+
+        public string MaghribBegin { get; set; }
+
+        public string MaghribJamaah { get; set; }
+
+        public string IshaBegin { get; set; }
+
+        public string IshaJamaah { get; set; }
+    }
+}
